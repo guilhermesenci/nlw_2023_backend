df0dd935af0c24cb27cd5f70943982c27574b0f0
Guilherme Senci <guilherme@ephrom.com.br>
2023-05-18 23:24:45 -0300

    aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 /* eslint-disable prettier/prettier */
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import cors from '@fastify/cors'
+import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
-app.get("/users", async () => {
-  const users = await prisma.user.findMany()
-
-  return users;
-});
+app.register(cors, {
+  origin: true, // todas as urls do front poderao acessar o backend
+})
+app.register(memoriesRoutes)
 
 app
   .listen({
